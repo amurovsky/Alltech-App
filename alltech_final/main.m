4a06ca3a4acf0d9821d192f4e995f4487a03b3f6
Tejuino developers <tejuinodevelopers@Tejuinos-iMac.local>
2015-02-04 12:36:23 -0600

    Initial Commit

---
+++ alltech_final/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  alltech_final
+//
+//  Created by Tejuino developers on 04/02/15.
+//  Copyright (c) 2015 Tejuino developers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
